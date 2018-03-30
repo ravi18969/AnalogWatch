543f0e11fdf5b1c01e25095566bf51263e1c93fc
ravi18969 <ravi189690@gmail.com>
2018-03-31 04:12:54 +0530

    All commited

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/analog.html");
+});
+
+//Rotating  watch hands
+function tick() {
+    var date = new Date();
+    var seconds = date.getSeconds();
+    var minutes = date.getMinutes();
+    var hours = date.getHours();
+
+    var value = {
+        secAngle: seconds * 6,
+        minAngle: minutes * 6 + seconds * (360 / 3600),
+        hourAngle: hours * 30 + minutes * (360 / 720)
+    };
+
+    return value;
+}
+
+
+io.on("connection", function(client) {
+    client.on("time", function(data) {
+        var result = tick();
+        io.sockets.emit("time", result);
+    });
+});
+
+
+server.listen(process.env.PORT || 8000);
+console.log(`server is running....`);
